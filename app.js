0d1f25663620e98a0be0a62332b5ffe7b407e7d4
Javier Farre <jfarre90@gmail.com>
2018-12-08 11:57:35 +0000

    debugging database issue

--- app.js
+++ app.js
@@ -18,7 +18,8 @@ var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index")
     
-mongoose.connect("mongodb://localhost/yelp_camp_v9");
+var url = process.env.DATABASEURL || "mongodb://localhost/yelp_camp_v10";
+mongoose.connect(url);
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
