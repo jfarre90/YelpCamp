a31c061a301b06e52323695ad2dda21c9beb824f
Javier Farre <jfarre90@gmail.com>
2019-03-04 11:37:23 +0000

    added text input in the form to add the cost for each of the campgrounds

--- models/campground.js
+++ models/campground.js
@@ -4,6 +4,7 @@ var campgroundSchema = new mongoose.Schema({
    name: String,
    image: String,
    description: String,
+   cost: Number,
    author: {
       id: {
          type: mongoose.Schema.Types.ObjectId,
